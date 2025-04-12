71f70373b9d13958e0f6b5dbe8998d5ece955467
pdolar <kolejnydolar@gmail.com>
2025-04-12 11:41:17 +0200

    Update readLoginPage.ts

--- zadania/s01e01/readLoginPage.ts
+++ zadania/s01e01/readLoginPage.ts
@@ -16,22 +16,7 @@ export async function strzelaj() : Promise<string> {
     //const data = await readData.text();
     //console.log("+++++++++++++++++++++++++++++++++++++"+'\n' );
     console.log(specificInput?.textContent);
-/*
-    const sendData = await fetch('https://poligon.aidevs.pl/verify', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },  
-      body: JSON.stringify({ 
-        task: "POLIGON",
-        apikey: "95db6f7e-9c64-40d9-81e0-ce380f8cb498",
-        answer: [lines[0],lines[1]]
-      })
-    });
 
-    const data2 = await sendData.text();
-    console.log("data2:"+'\n'+data2); 
-*/
     return specificInput?.textContent || "";
   } catch (error) {
     console.error('Error:', error);
